b29a3a1971184a79d975044688a1e4233ba82038
iman cheema <cheema.iman2@gmail.com>
2023-12-29 14:54:19 -0500

    Adds dashboard + styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import LandingPage from './screens/LandingPage';
-import Dashboard from './screens/Dashboard';
-import NavBar from './components/NavBar';
-import LoadManagement from './screens/LoadManagement';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./screens/LandingPage";
+import Dashboard from "./screens/Dashboard";
+import NavBar from "./components/NavBar";
+import LoadManagement from "./screens/LoadManagement";
 import ProfileManagement from "./screens/ProfileManagement";
 
 function App() {
@@ -11,10 +11,27 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dash" element={<Dashboard />} />
-        <Route path = "/Nav" element={<NavBar />} />
-        <Route path = "/LoadManagement" element={<LoadManagement />} />
-        <Route path = "/Profile" element={<ProfileManagement />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/load-management" element={<LoadManagement />} />
+        <Route path="/profile" element={<ProfileManagement />} />
+        <Route
+          path="/energy-analysis"
+          element={
+            <>
+              <NavBar />
+              <p> Not yet implemented</p>
+            </>
+          }
+        />
+        <Route
+          path="/schedule"
+          element={
+            <>
+              <NavBar />
+              <p> Not yet implemented</p>
+            </>
+          }
+        />
       </Routes>
     </Router>
   );
--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,83 +1,86 @@
-import React from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import "./Dashboard.css";
+import NavBar from "../components/NavBar";
 
 const Dashboard: React.FC = () => {
   const stateOfCharge = 33;
   const temperature = 94;
   const powerGeneration = 40;
+  const powerConsumption = 28;
 
   return (
-    <div className="dashboard">
-      <h1>Dashboard</h1>
-      <p>
-        Welcome to the Dashboard, the central hub for your energy journey. In real-time,
-        monitor critical metrics such as State of Charge (SoC), temperature, and power dynamics
-        to gain a comprehensive analysis of your battery storage system.
-      </p>
-      <div className="battery-storage-system-status">
-        <h2>Battery Storage System Status</h2>
-        <div className="card-container">
-          <div className="card">
-            <h3>State of Charge</h3>
-            <div className="progress-bar">
-              <CircularProgressbar
-                value={stateOfCharge}
-                text={`${stateOfCharge}%`}
-                className="progress-bar-state-of-charge"
-                strokeWidth={10}
-                styles={buildStyles({
-                  textColor: '#333',
-                  pathTransition: 'none',
-                  trailColor: '#eee',
-                })}
-              />
+    <>
+      <NavBar />
+      <div className="dashboard">
+        <h1>Dashboard</h1>
+        <p>
+          Welcome to the Dashboard, the central hub for your energy journey. In
+          real-time, monitor critical metrics such as State of Charge (SoC),
+          temperature, and power dynamics to gain a comprehensive analysis of
+          your battery storage system.
+        </p>
+        <div>
+          <h2>Battery Storage System Status</h2>
+          <div className="card-container">
+            <div className="card">
+              <h3>State of Charge</h3>
+              <div className="progress-bar">
+                <CircularProgressbar
+                  value={stateOfCharge}
+                  text={`${stateOfCharge}%`}
+                  className="progress-bar-state-of-charge"
+                  strokeWidth={10}
+                  styles={buildStyles({
+                    textColor: "#333",
+                    pathTransition: "none",
+                    trailColor: "#eee",
+                  })}
+                />
+              </div>
+              <h4>HEALTH INDICATOR</h4>
+              <p>The battery is in a moderate state</p>
             </div>
-            <h4>HEALTH INDICATOR</h4>
-            <p>The battery is in a moderate state</p>
-          </div>
 
-          <div className="card">
-            <h3>Temperature</h3>
-            <div className="progress-bar">
-              <CircularProgressbar
-                value={temperature}
-                text={`${temperature}°C`}
-                className="progress-bar-temperature"
-                strokeWidth={10}
-                styles={buildStyles({
-                  textColor: '#333',
-                  pathTransition: 'none',
-                  trailColor: '#eee',
-                })}
-              />
+            <div className="card">
+              <h3>Temperature</h3>
+              <div className="progress-bar">
+                <CircularProgressbar
+                  value={temperature}
+                  text={`${temperature}°C`}
+                  className="progress-bar-temperature"
+                  strokeWidth={10}
+                  styles={buildStyles({
+                    textColor: "#333",
+                    pathTransition: "none",
+                    trailColor: "#eee",
+                  })}
+                />
+              </div>
+              <h4>CURRENT TEMPERATURE</h4>
+              <p>The battery is in a normal state</p>
             </div>
-            <h4>Current Temperature</h4>
-            <p>The battery is in a normal state</p>
-          </div>
 
-          <div className="card">
-            <h3>Power Metrics</h3>     
-            <div className="progress-bar">
-              <CircularProgressbar
-                value={powerGeneration}
-                text={`${powerGeneration}%`}
-                className="progress-bar-power"
-                strokeWidth={10}
-                styles={buildStyles({
-                  textColor: '#333',
-                  pathTransition: 'none',
-                  trailColor: '#eee',
-                })}
-              />
+            <div className="card">
+              <h3>Power Metrics</h3>
+              <div className="metric">
+                <h4>Power Generation</h4>
+                <p className="metric-value">{powerGeneration} kW</p>
+              </div>
+              <div className="metric">
+                <h4>Power Consumption</h4>
+                <p className="metric-value">{powerConsumption} kW</p>
+              </div>
+              <h4>POWER STATE</h4>
+              <div className="efficiency-status">
+                <p>The system is operating efficiently</p>
+              </div>
             </div>
-            <h4>POWER STATE</h4>
-            <p>The system is operating efficiently</p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
