0f0c875f22a5f1c2da3fa52fcc1ca5a722d1f70e
iman cheema <cheema.iman2@gmail.com>
2024-01-04 00:28:41 -0500

    Adds batteries, measurements service + batteries controller

---
+++ controllers/batteries.controller.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.status(200).send({
+    status: "Success",
+    message: `Not Implemented yet`,
+  });
+});
+
+// create battery
+// get all batteries
+// get specific battery by id
+// update battery details
+// delete battery
+// get measurements by batteryId
+// get measurements by batteryId and time
+
+module.exports = router;
