5746cc6386e4a81583ff33ad69d910da88eb0646
Steven <quachsteven@hotmail.com>
2024-01-09 12:38:57 -0500

    Readded styles

--- src/screens/LoadManagement.tsx
+++ src/screens/LoadManagement.tsx
@@ -6,57 +6,110 @@ import { useState } from "react";
 const LoadManagement = () => {
   const [openModal, setOpenModal] = useState(false);
 
-    return(
-        <body className="LoadManagement">
-            <div>
-                <Navbar />
-            </div>
-            <div id ="main" className="main">
-                <div className="PageHeader">Load Management</div>
-                <div className="PageText">Take full control over of your energy usage. Add, edit, or delete loads, and create personalized schedules for each to ensure optimal efficiency.</div>
-                <table className="LoadTable">
-                    <tr>
-                        <td>
-                            <div className ="Load-Head" id="Load-Head1">Heater</div>
-                            <div className="Load-Value" id="Load-Value">1.5kWh</div>
-                            <div className="Options"><button className="Edit">Edit</button><button className="Delete">Delete</button></div>
-                        </td>
-                        <td>
-                            <div className ="Load-Head" id="Load-Head1">Heater</div>
-                            <div className="Load-Value" id="Load-Value">1.5kWh</div>
-                            <div className="Options"><button className="Edit">Edit</button><button className="Delete">Delete</button></div>
-                        </td>
-                        <td>
-                            <div className ="Load-Head" id="Load-Head1">Heater</div>
-                            <div className="Load-Value" id="Load-Value">1.5kWh</div>
-                            <div className="Options"><button className="Edit">Edit</button><button className="Delete">Delete</button></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div className ="Load-Head" id="Load-Head1">Heater</div>
-                            <div className="Load-Value" id="Load-Value">1.5kWh</div>
-                            <div className="Options"><button className="Edit">Edit</button><button className="Delete">Delete</button></div>
-                        </td>
-                        <td>
-                            <div className ="Load-Head" id="Load-Head1">Heater</div>
-                            <div className="Load-Value" id="Load-Value">1.5kWh</div>
-                            <div className="Options"><button className="Edit">Edit</button><button className="Delete">Delete</button></div>
-                        </td>
-                        <td>
-                            <div className ="Load-Head" id="Load-Head1">Heater</div>
-                            <div className="Load-Value" id="Load-Value">1.5kWh</div>
-                            <div className="Options"><button className="Edit">Edit</button><button className="Delete">Delete</button></div>
-                        </td>
-                    </tr>
-                </table>
-                <div className="Display">
-                    <button className="Add" onClick={()=> {setOpenModal(true)}}>+</button><span className="AddText">Add new load</span>
-                    {openModal && <AddLoad closeModal={setOpenModal} />}
-                </div>
-            </div>
-        </body>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="load-management-bg"></div>
+      <div id="load-management" className="load-management">
+        <h1>Load Management</h1>
+        <p>
+          Take full control over of your energy usage. Add, edit, or delete
+          loads, and create personalized schedules for each to ensure optimal
+          efficiency.
+        </p>
+        <table className="LoadTable">
+          <tr>
+            <td>
+              <div className="Load-Head" id="Load-Head1">
+                Heater
+              </div>
+              <div className="Load-Value" id="Load-Value">
+                1.5kWh
+              </div>
+              <div>
+                <button className="Edit">Edit</button>
+                <button className="Delete">Delete</button>
+              </div>
+            </td>
+            <td>
+              <div className="Load-Head" id="Load-Head1">
+                Heater
+              </div>
+              <div className="Load-Value" id="Load-Value">
+                1.5kWh
+              </div>
+              <div>
+                <button className="Edit">Edit</button>
+                <button className="Delete">Delete</button>
+              </div>
+            </td>
+            <td>
+              <div className="Load-Head" id="Load-Head1">
+                Heater
+              </div>
+              <div className="Load-Value" id="Load-Value">
+                1.5kWh
+              </div>
+              <div>
+                <button className="Edit">Edit</button>
+                <button className="Delete">Delete</button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="Load-Head" id="Load-Head1">
+                Heater
+              </div>
+              <div className="Load-Value" id="Load-Value">
+                1.5kWh
+              </div>
+              <div>
+                <button className="Edit">Edit</button>
+                <button className="Delete">Delete</button>
+              </div>
+            </td>
+            <td>
+              <div className="Load-Head" id="Load-Head1">
+                Heater
+              </div>
+              <div className="Load-Value" id="Load-Value">
+                1.5kWh
+              </div>
+              <div>
+                <button className="Edit">Edit</button>
+                <button className="Delete">Delete</button>
+              </div>
+            </td>
+            <td>
+              <div className="Load-Head" id="Load-Head1">
+                Heater
+              </div>
+              <div className="Load-Value" id="Load-Value">
+                1.5kWh
+              </div>
+              <div>
+                <button className="Edit">Edit</button>
+                <button className="Delete">Delete</button>
+              </div>
+            </td>
+          </tr>
+        </table>
+        <div className="Display">
+          <button
+            className="Add"
+            onClick={() => {
+              setOpenModal(true);
+            }}
+          >
+            +
+          </button>
+          <span className="AddText">Add new load</span>
+          {openModal && <AddLoad closeModal={setOpenModal} />}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoadManagement;
--- src/screens/ProfileManagement.tsx
+++ src/screens/ProfileManagement.tsx
@@ -21,15 +21,15 @@ const ProfileManagement = () => {
             <div id ="main" className="main">
                 <div className="PageHeader">Edit Profile</div>
                 <form className="Information">
-                    <label>First Name {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</label><label className="end">Last Name</label>
+                    <label>First Name {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</label><label className="end">Last Name</label>
                     <br></br>
                     <input type="text" className="halfbox"></input><input type="text" className="halfbox"></input>
                     <br></br><br></br>
-                    <label>Email {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</label><label className="end">Contact Number</label>
+                    <label>Email {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</label><label className="end">Contact Number</label>
                     <br></br>
                     <input type="text" className="halfbox"></input><input type="text" className="halfbox"></input>
                     <br></br><br></br>
-                    <label>City{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</label><label className="end">Postal Code</label>
+                    <label>City{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</label><label className="end">Postal Code</label>
                     <br></br>
                     <input type="text" className="halfbox"></input><input type="text" className="halfbox"></input>
                     <br></br><br></br>
