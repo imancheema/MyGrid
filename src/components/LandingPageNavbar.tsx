2aa034f8ff90350656a41314b2f6b53c2396d18e
iman cheema <cheema.iman2@gmail.com>
2024-04-05 02:03:56 -0400

    Adds logo to landing page navbar

--- src/components/LandingPageNavbar.tsx
+++ src/components/LandingPageNavbar.tsx
@@ -1,9 +1,13 @@
+import React from "react";
+import logo from "../assets/Logo.png"; // Import your logo image
 import "./LandingPageNavbar.css";
 
 const LandingPageNavbar = () => {
   return (
     <div className="landing-page-navbar">
-      <div className="logo">Logo</div>
+      <div className="logo">
+        <img src={logo} alt="Logo" /> {/* Display your logo */}
+      </div>
       <div className="auth-buttons">
         <a href="/Login">
           <button className="login-button">Login</button>
