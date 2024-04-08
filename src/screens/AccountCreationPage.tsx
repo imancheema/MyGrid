94fb9f2bb50fbf8cb4d8d134a0d3dbff90845187
iman cheema <cheema.iman2@gmail.com>
2024-04-08 00:56:25 -0400

    Updates login and registration

--- src/screens/AccountCreationPage.tsx
+++ src/screens/AccountCreationPage.tsx
@@ -1,9 +1,11 @@
 import "./SimplePages.css";
-import React, {useState} from "react";
-import { UserCredential, createUserWithEmailAndPassword, sendEmailVerification} from "firebase/auth";
+import React, { useState } from "react";
 import {
-  createUser
-} from "../frontend-services/accountCreation.service.ts";
+  UserCredential,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import { createUser } from "../frontend-services/accountCreation.service.ts";
 import firebase from "../../firebase.js";
 
 //TODO Validation for proper email, phone number, postal code
@@ -20,10 +22,9 @@ const LoginPage = () => {
     postalCode: "",
   });
 
-  const [emailErr, setEmailErr] = useState(true)
-  const [weakPassErr, setWeakPassErr] = useState(true)
-  const [invPassErr, setInvPassErr] = useState(true)
-  
+  const [emailErr, setEmailErr] = useState(true);
+  const [weakPassErr, setWeakPassErr] = useState(true);
+  const [invPassErr, setInvPassErr] = useState(true);
 
   const addUser = async () => {
     try {
@@ -32,94 +33,186 @@ const LoginPage = () => {
     } catch (error) {
       console.log("Error creating user:", error);
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
     const auth = firebase.auth;
     e.preventDefault();
-    
-    setEmailErr(true)
-    setWeakPassErr(true)
-    setInvPassErr(true)
 
-    if (userInfo.password === userInfo.confirmPassword){
+    setEmailErr(true);
+    setWeakPassErr(true);
+    setInvPassErr(true);
+
+    if (userInfo.password === userInfo.confirmPassword) {
       try {
-      const newUserCredential: UserCredential = await createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
-      await sendEmailVerification(newUserCredential.user);
-      await addUser();
-      window.location.href = "http://localhost:5173/Login";
+        const newUserCredential: UserCredential =
+          await createUserWithEmailAndPassword(
+            auth,
+            userInfo.email,
+            userInfo.password
+          );
+        await sendEmailVerification(newUserCredential.user);
+        await addUser();
+        window.location.href = "http://localhost:5173/Login";
       } catch (error) {
-        if (error.code === "auth/invalid-email" || error.code === "auth/email-already-exists"){
+        if (
+          error.code === "auth/invalid-email" ||
+          error.code === "auth/email-already-exists"
+        ) {
           setEmailErr(false);
         }
-        if (error.code === "auth/weak-password" || error.code === "auth/missing-password"){
-          setWeakPassErr(false)
+        if (
+          error.code === "auth/weak-password" ||
+          error.code === "auth/missing-password"
+        ) {
+          setWeakPassErr(false);
         }
         console.log(error);
       }
-    }
-    else {
+    } else {
       setInvPassErr(false);
     }
-  }
-  
-    return(
-      <body className="login-body">
-        <div>
-          <nav className="account-nav">
-            <div className="logo">Logo</div>
-          </nav>
-          <form onSubmit= {handleSubmit} className="info-container">
-            <div className="title-text">
-              <p>Welcome</p>
-              <h1>Create your account</h1>
-            </div>
-            <label htmlFor="email">Email</label>
-            <br/>
-            <input className="info-input" type="text" id="email" name="email" value={userInfo.email} onChange={(e) => setUserInfo({...userInfo, email: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">Password</label>
-            <br/>
-            <input className="info-input" type="password" id="password" name="password" value={userInfo.password} onChange={(e) => setUserInfo({...userInfo, password: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">Confirm Password</label>
-            <br/>
-            <input className="info-input" type="password" id="confirm-password" name="confirm-password" value={userInfo.confirmPassword} onChange={(e) => setUserInfo({...userInfo, confirmPassword: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">First Name</label>
-            <br/>
-            <input className="info-input" type="text" id="firstName" name="firstName" value={userInfo.firstName} onChange={(e) => setUserInfo({...userInfo, firstName: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">Last Name</label>
-            <br/>
-            <input className="info-input" type="text" id="lastName" name="lastName" value={userInfo.lastName} onChange={(e) => setUserInfo({...userInfo, lastName: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">Phone Number</label>
-            <br/>
-            <input className="info-input" type="text" id="phoneNum" name="phoneNum" value={userInfo.phoneNum} onChange={(e) => setUserInfo({...userInfo, phoneNum: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">City</label>
-            <br/>
-            <input className="info-input" type="text" id="city" name="city" value={userInfo.city} onChange={(e) => setUserInfo({...userInfo, city: e.target.value})}></input>
-            <br/>
-            <label htmlFor="password">Postal Code</label>
-            <br/>
-            <input className="info-input" type="text" id="postalCode" name="postalCode" value={userInfo.postalCode} onChange={(e) => setUserInfo({...userInfo, postalCode: e.target.value})}></input>
-            <br/>
-            <div hidden={emailErr} className="invalid-text" id="invalid-email">Please enter a valid email</div>
-            <div hidden={weakPassErr} className="invalid-text" id="weak-password">Please enter a password longer than 6 characters</div>
-            <div hidden={invPassErr} className="invalid-text" id="invalid-password">Passwords do not match</div>
-            <br/>
-            <a> 
-              <button type="submit" className="account-button">Register Now</button>
-            </a>
-          </form>
-            <p className="centered-element">Already have an account?{' '}
-              <a href="/Login">Login.</a>
-            </p>
-        </div>
-      </body>
-    );
   };
-  
-  export default LoginPage;
+
+  return (
+    <body className="login-body">
+      <div>
+        <nav className="account-nav">
+          <div className="logo">Logo</div>
+        </nav>
+        <form onSubmit={handleSubmit} className="info-container">
+          <div className="title-text">
+            <p>Welcome</p>
+            <h1>Create your account</h1>
+          </div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="email"
+            name="email"
+            value={userInfo.email}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, email: e.target.value })
+            }
+          ></input>
+          <br />
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            className="info-input"
+            type="password"
+            id="password"
+            name="password"
+            value={userInfo.password}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, password: e.target.value })
+            }
+          ></input>
+          <br />
+          <label htmlFor="password">Confirm Password</label>
+          <br />
+          <input
+            className="info-input"
+            type="password"
+            id="confirm-password"
+            name="confirm-password"
+            value={userInfo.confirmPassword}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, confirmPassword: e.target.value })
+            }
+          ></input>
+          <br />
+          <label htmlFor="password">First Name</label>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={userInfo.firstName}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, firstName: e.target.value })
+            }
+          ></input>
+          <br />
+          <label htmlFor="password">Last Name</label>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={userInfo.lastName}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, lastName: e.target.value })
+            }
+          ></input>
+          <br />
+          <label htmlFor="password">Phone Number</label>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="phoneNum"
+            name="phoneNum"
+            value={userInfo.phoneNum}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, phoneNum: e.target.value })
+            }
+          ></input>
+          <br />
+          <label htmlFor="password">City</label>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="city"
+            name="city"
+            value={userInfo.city}
+            onChange={(e) => setUserInfo({ ...userInfo, city: e.target.value })}
+          ></input>
+          <br />
+          <label htmlFor="password">Postal Code</label>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="postalCode"
+            name="postalCode"
+            value={userInfo.postalCode}
+            onChange={(e) =>
+              setUserInfo({ ...userInfo, postalCode: e.target.value })
+            }
+          ></input>
+          <br />
+          <div hidden={emailErr} className="invalid-text" id="invalid-email">
+            Please enter a valid email
+          </div>
+          <div hidden={weakPassErr} className="invalid-text" id="weak-password">
+            Please enter a password longer than 6 characters
+          </div>
+          <div
+            hidden={invPassErr}
+            className="invalid-text"
+            id="invalid-password"
+          >
+            Passwords do not match
+          </div>
+          <br />
+          <a>
+            <button type="submit" className="account-button">
+              Register Now
+            </button>
+          </a>
+          <p className="centered-element">
+            Already have an account? <a href="/Login">Login.</a>
+          </p>
+        </form>
+      </div>
+    </body>
+  );
+};
+
+export default LoginPage;
--- src/screens/LoginPage.tsx
+++ src/screens/LoginPage.tsx
@@ -1,87 +1,113 @@
 import "./SimplePages.css";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import firebase from "../../firebase.js";
+import logo from "../assets/Logo.png";
 
-import {
-  getUserByEmail
-} from "../frontend-services/accountCreation.service.ts";
+import { getUserByEmail } from "../frontend-services/accountCreation.service.ts";
 import { useState } from "react";
-
+import LandingPageNavbar from "../components/LandingPageNavbar.tsx";
 
 const LoginPage = () => {
-  const [showCredErr, setShowCredErr] = useState(true)
-  const [showVerifyErr, setShowVerifyErr] = useState(true)
+  const [showCredErr, setShowCredErr] = useState(true);
+  const [showVerifyErr, setShowVerifyErr] = useState(true);
 
   const handleLogin = async (e) => {
     const auth = firebase.auth;
     var emailElement = document.getElementById("email");
     var passwordElement = document.getElementById("password");
-    let password: string
-    let email: string
-  
-    if (emailElement instanceof HTMLInputElement){
+    let password: string;
+    let email: string;
+
+    if (emailElement instanceof HTMLInputElement) {
       email = emailElement.value;
     } else {
-      email = ""
+      email = "";
     }
-  
-    if (passwordElement instanceof HTMLInputElement){
+
+    if (passwordElement instanceof HTMLInputElement) {
       password = passwordElement.value;
     } else {
-      password = ""
+      password = "";
     }
     e.preventDefault();
 
-    setShowCredErr(true)
-    setShowVerifyErr(true)
+    setShowCredErr(true);
+    setShowVerifyErr(true);
 
-    try{
-      const userCredential = await signInWithEmailAndPassword(auth, email, password)
-          const user = userCredential.user;
-          if(user.emailVerified){
-            const response = await getUserByEmail(email)
-            sessionStorage.setItem("user", JSON.stringify(response.data.user[0]));
-            window.location.href = "http://localhost:5173/dashboard";
-          }
-          else{
-            setShowVerifyErr(false)
-          }
-          // ...
-        } catch (error) {
-          if(error.code === "auth/invalid-email" || error.code === "auth/missing-password" || error.code === "auth/invalid-credential"){
-            setShowCredErr(false)
-          }
-          console.log(error)
-        }
-  }
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      if (user.emailVerified) {
+        const response = await getUserByEmail(email);
+        sessionStorage.setItem("user", JSON.stringify(response.data.user[0]));
+        window.location.href = "http://localhost:5173/dashboard";
+      } else {
+        setShowVerifyErr(false);
+      }
+      // ...
+    } catch (error) {
+      if (
+        error.code === "auth/invalid-email" ||
+        error.code === "auth/missing-password" ||
+        error.code === "auth/invalid-credential"
+      ) {
+        setShowCredErr(false);
+      }
+      console.log(error);
+    }
+  };
 
-  return(
+  return (
     <body className="login-body">
       <div>
-        <nav className="account-nav">
-          <div className="logo">Logo</div>
-        </nav>
+        <nav className="account-nav"></nav>
         <form onSubmit={handleLogin} className="info-container">
           <div className="title-text">
             <p>Welcome Back</p>
             <h1>Login to your account</h1>
           </div>
           <label htmlFor="email">Email</label>
-          <br/>
-          <input className="info-input" type="text" id="email" name="email" placeholder="email"></input>
-          <br/>
+          <br />
+          <input
+            className="info-input"
+            type="text"
+            id="email"
+            name="email"
+            placeholder="email"
+          ></input>
+          <br />
           <label htmlFor="password">Password</label>
-          <br/>
-          <input className="info-input" type="password" id="password" name="password" placeholder="password"></input>
-          <br/>
-          <div hidden={showCredErr} className="invalid-text" id="invalid-creds">Invalid credentials please try again</div>
-          <div hidden={showVerifyErr} className="invalid-text" id="not-verified">Please verify your email and try again</div>
-          <br/>
+          <br />
+          <input
+            className="info-input"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="password"
+          ></input>
+          <br />
+          <div hidden={showCredErr} className="invalid-text" id="invalid-creds">
+            Invalid credentials please try again
+          </div>
+          <div
+            hidden={showVerifyErr}
+            className="invalid-text"
+            id="not-verified"
+          >
+            Please verify your email and try again
+          </div>
+          <br />
           <a href="">Forgot Password?</a>
-          <br/>
-          <button type ="submit" className="account-button">Login Now</button>
-          <p>Don't have an account?{' '}
-            <a href="/AccountCreate">Create one now.</a>
+          <br />
+          <button type="submit" className="account-button">
+            Login Now
+          </button>
+          <p>
+            Don't have an account? <a href="/AccountCreate">Create one now.</a>
           </p>
         </form>
       </div>
