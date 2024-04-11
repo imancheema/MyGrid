0e4523d559cc2a4581744e9478ced9eb764c0c4b
iman cheema <cheema.iman2@gmail.com>
2024-04-11 00:47:17 -0400

    Remove reference to localhost in form action

--- src/screens/UserAuthPage.tsx
+++ src/screens/UserAuthPage.tsx
@@ -2,30 +2,35 @@ import "./SimplePages.css";
 import logo from "../assets/Logo.png";
 
 const UserAuthPage = () => {
-    return (
-      <body className="login-body">
-        <div>
-          <nav className="logo">
-            <img src={logo} alt="logo"/>
-          </nav>
-          <form className="info-container">
-            <div className="title-text">
-              <h1>One Final Step</h1>
-              <img src="src\assets\VerificationSuccess.png" alt="Verification Success"></img>
-              <h2>Check your email</h2>
-              <p>Thank you for registering your account with us, we have sent <br/>
-                  a one time verification to your email. <br/> Please check your 
-                  device and click the link to activate your account.
-              </p>
-            </div>
-          </form>
-          <form action="http://localhost:5173/Login">
-            <button className="account-button">Continue</button>
-          </form>
-            <br/>
-        </div>
-      </body>
-    );
-  };
-  
-  export default UserAuthPage;
+  return (
+    <body className="login-body">
+      <div>
+        <nav className="logo">
+          <img src={logo} alt="logo" />
+        </nav>
+        <form className="info-container">
+          <div className="title-text">
+            <h1>One Final Step</h1>
+            <img
+              src="src\assets\VerificationSuccess.png"
+              alt="Verification Success"
+            ></img>
+            <h2>Check your email</h2>
+            <p>
+              Thank you for registering your account with us, we have sent{" "}
+              <br />
+              a one time verification to your email. <br /> Please check your
+              device and click the link to activate your account.
+            </p>
+          </div>
+        </form>
+        <form action="/Login">
+          <button className="account-button">Continue</button>
+        </form>
+        <br />
+      </div>
+    </body>
+  );
+};
+
+export default UserAuthPage;
